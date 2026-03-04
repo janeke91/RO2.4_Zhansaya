af39567b136d1cf91da9480e4a0fa848ce2bdeb0
Zhansaya Atlas <zhansayaatlas@gmail.com>
2026-03-04 23:47:01 +0500

    Add files via upload

---
+++ Exercise2.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter any number:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 5 && num < 10)
+{
+    Console.WriteLine("The number is greater than 5 and less than 10");
+}
+ else
+{
+    Console.WriteLine("Unknown number");
+}
