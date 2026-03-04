7417adda83075246969d756f43f470d78eaee3d2
Zhansaya Atlas <zhansayaatlas@gmail.com>
2026-03-04 23:48:12 +0500

    Add files via upload

---
+++ Exercise1.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter the first number:");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number:");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 > n2)
+{
+    Console.WriteLine("First entered number is larger than the second");
+}
+else if (n2 > n1)
+{
+    Console.WriteLine("Second entered number is larger than the first");
+}
+else
+{
+    Console.WriteLine("Two entered numbers are equal :)");
+}
